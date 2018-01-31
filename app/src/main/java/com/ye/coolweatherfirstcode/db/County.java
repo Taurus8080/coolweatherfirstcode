46625f4a5862ba4608f52f7b45bdd4efac6e07a5
Taurus <150025975@qq.com>
2018-01-31 15:26:36 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/ye/coolweatherfirstcode/db/County.java
@@ -0,0 +1,47 @@
+package com.ye.coolweatherfirstcode.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Taurus on 18.1.31.
+ * 县信息类
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName; // 县名
+    private String weatherId;  // 县对应的天气id
+    private int cityId;  // 所属市id
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
